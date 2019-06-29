c4f05c03e900ee2cea6397b263866b606cdf4a93
AtilioA <atiliodadalto@hotmail.com>
2019-06-29 17:30:39 -0300

    Organizaçãozinha da main

--- trab2-AtilioDadalto-TiagoSantos.c
+++ trab2-AtilioDadalto-TiagoSantos.c
@@ -2,13 +2,14 @@
 #include "include/arvore_binaria.h"
 #include "include/arvore_AVL.h"
 #include "include/indexador.h"
+#include "include/arquivos.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <sys/time.h>
 
-/*
+/* !
  * CONVENÇÕES PARA O TRABALINDO:
  * Constantes em CAPS: UNUSED, TRUE, FALSE.
  * Structs e typedefs em CamelCase: ArvBin, Lista.
@@ -16,39 +17,63 @@
  * Ponteiros com asterisco do lado do nome da variável: ArvBin *foo, int *bar.
  */
 
+
 int main(int argc, char *argv[])
 {
-    //clr_scr();
+    clr_scr();
 
+    printf("Criando estruturas...\n");
     struct timeval t;
     Lista *lista = cria_Lista();
     ArvBin *arvere = criaArvBin();
     ArvAVL *arvl = cria_ArvAVL();
-    tSentRandPal *pal_al = newRandpal(); //lista de busca
+    printf("Estruturas criadas com sucesso.\n");
+
+    tSentRandPal *pal_al = newRandpal(); // lista de busca de palavras
     Palavra *pal;
     char strat[80];
-    int kkkeae;
-    FILE *f = fopen(argv[1], "r");
-    int k = atoi(argv[2]);
-    char vet[k][80]; //vetor de busca
+    int posicao;
+    printf("Lista de busca de palavras criada com sucesso.\n");
+
+
+    FILE *f = le_arquivo(argv[1]);
+
+    if (argv[2] == NULL)
+    {
+        printf("Entrada de buscas invalida.\n");
+        exit(1);
+    }
+
+    int nBuscas = atoi(argv[2]);
+    if (nBuscas < 1)
+    {
+        printf("Numero de buscas invalido.\n");
+        exit(1);
+    }
+    printf("Arquivo aberto com sucesso.\n");
+
+    char vet[nBuscas][80]; // vetor de busca
 
     gettimeofday(&t, NULL);
     srand((unsigned int)t.tv_usec);
 
+    printf("Lendo arquivo...\n");
     while(!feof(f)){
         fscanf(f, "%s", strat); // vo usar o fscanf de placeholder até a gnt fazer uma função de leitura de palavras decente
         insereRandPal(strat, pal_al);
-        kkkeae = ftell(f) - strlen(strat) + 1;
-        pal = cria_Palavra(strat, kkkeae);
+        posicao = ftell(f) - strlen(strat) + 1;
+        pal = cria_Palavra(strat, posicao);
         insere_ArvBin(arvere, pal);
-        pal = cria_Palavra(strat, kkkeae);
+        pal = cria_Palavra(strat, posicao);
         insere_Lista(pal, lista);
-        pal = cria_Palavra(strat, kkkeae);
+        pal = cria_Palavra(strat, posicao);
         insere_ArvAVL(arvl, pal);
     }
-    for(int i = 0; i < k; i++){
-        strcpy(vet[i], buscaRandPal(rand()%(pal_al->qtd), pal_al)); //carrega o vetor com palavras aleatórias da lista
+    for(int i = 0; i < nBuscas; i++){
+        strcpy(vet[i], buscaRandPal(rand()%(pal_al->qtd), pal_al)); // carrega o vetor com palavras aleatórias da lista
     }
+
+    printf("\n\nLEITURA CONCLUIDA. IMPRIMINDO RESULTADOS:");
 
     printf("\n\n\nARVORE BINARIA (em ordem)\n\n\n");
     em_ordem_ArvBin(arvere);
@@ -56,7 +81,7 @@ int main(int argc, char *argv[])
     em_ordem_ArvAVL(arvl);
     printf("\n\n\nLISTA ENCADEADA\n\n\n");
     imprime_lista(lista);
-    for(int i = 0; i < k; i++){
+    for(int i = 0; i < nBuscas; i++){
         Palavra* seu = busca_Lista(vet[i], lista);
         Palavra *meu = consulta_ArvBin(arvere, vet[i]);
         Palavra *nosso = consulta_ArvAVL(arvl, vet[i]);
@@ -70,12 +95,16 @@ int main(int argc, char *argv[])
             printf("A palavra %s foi encontrada na arvore avl\n", vet[i]);
         }
     }
-    printf("\n");
+
+    printf("\nFim da leitura. Liberando estruturas...\n");
     destroiRandPal(pal_al);
     //imprime_lista(lista);
     fclose(f);
     libera_ArvBin(arvere);
     libera_ArvAVL(arvl);
     destroi_lista(lista);
+
+    printf("Estruturas liberadas com sucesso. Talvez.\n");
+
     return 0;
 }
