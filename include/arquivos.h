1869b518e7e5420b1a8e0d2aad92d56a9b941e92
AtilioA <atiliodadalto@hotmail.com>
2019-06-29 17:30:19 -0300

    Função para abertura de arquivo

--- include/arquivos.h
+++ include/arquivos.h
@@ -1,6 +1,14 @@
+/* Esta biblioteca contém o código que implementa funções mais básicas
+ * relevantes à abertura e manipulação de arquivos.
+ */
+
 // Guard para evitar dupla inclusão
 #ifndef __ARQUIVOS_H
 #define __ARQUIVOS_H
+
+#include <stdio.h>
+
+FILE *le_arquivo(char *string);
 
 int le_palavra(FILE *f, char *str);
 
--- src/arquivos.c
+++ src/arquivos.c
@@ -3,9 +3,58 @@
 #include <ctype.h>
 #include "../include/arquivos.h"
 #define _STRING_BUFFER_ 80
+#define UNUSED " \n^´"
+
+int charcmp(char c){
+    for(int i = 0; i < strlen(UNUSED); i++){
+        if(c == UNUSED[i]){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+char *lePoLePo(int t){
+    char *str = malloc(sizeof(char) * t);
+    char c;
+    for(int i = 0; i < t; i++){
+        scanf("%c", &c);
+        if(!charcmp(c)){
+            str[i] = c;
+        }else{
+            str[i] = '\0';
+            break;
+        }
+    }
+    return str;
+}
+
+/*
+int main(){
+    char c[80];
+    for(int i = 0; i < 10; i++){
+        strcpy(c, lePoLePo(80));
+        printf("%s ", c);
+    }
+    return 0;
+}
+*/
+
+FILE *le_arquivo(char *string)
+{
+    FILE *f = fopen(string, "r"); // abre apenas para leitura
+
+    if (f == NULL)
+    {
+        printf("Erro na leitura do arquivo.\n");
+        exit(1);
+    }
+
+    return f;
+}
+
 
 //dps a gnt faz outra
-
 int le_palavra(FILE *f, char *str)
 {
     int i = 0;
