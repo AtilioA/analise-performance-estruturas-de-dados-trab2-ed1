cc0b4aafe0a36c9887a95d13d110049fd34a3ab9
AtilioA <atiliodadalto@hotmail.com>
2019-06-30 16:30:43 -0300

    Começando árvore TRIE...

---
+++ include/arvore_TRIE.h
@@ -0,0 +1,13 @@
+/* Esta biblioteca contém o código que implementa funções mais básicas
+ * relevantes ao TAD árvore binária TRIE para o indexador de arquivos.
+ */
+
+// Guard para evitar dupla inclusão
+#ifndef __ARVORE_TRIE_H
+#define __ARVORE_TRIE_H
+
+#include "../include/indexador.h"
+
+int teste();
+
+#endif
